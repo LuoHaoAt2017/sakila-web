c4c1a5d659ba56d8d4dad5465a99810e41e8cb67
hao.luo01 <hao.luo01@ui-dh.com>
2024-11-29 15:17:05 +0800

    当 props 没有改变时跳过重新渲染

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,28 +1,24 @@
-import React, { useRef, lazy, Suspense } from "react";
+import { memo, useState } from "react";
 import { Button } from 'antd';
 
-const MarkdownPreview = lazy(function load() {
-  // 调用接口获取数据，根据数据来决定加载哪一个组件
-  return new Promise<{ default: React.ComponentType}>(function (resolve) {
-    setTimeout(() => {
-      resolve(import('@/components/MarkdownPreview'));
-    }, 3000);
-  });
+const Greeting = memo(function Ring() {
+  console.log('---child is render---');
+  return <div>this is ring</div>
 });
 
+
 export default function Home() {
-  const containerRef = useRef();
+  const [counter, setCounter] = useState(0);
 
-  const handleOpen = () => {
-
+  const handleClick = () => {
+    setCounter(counter + 1);
   }
-
+  console.log('---parent is render---');
   return (
-    <div ref={containerRef}>
-      <Button onClick={handleOpen}>Hello</Button>
-      <Suspense fallback={<div>Loading...</div>}>
-        <MarkdownPreview />
-      </Suspense>
+    <div>
+      <span>{counter + 1}</span>
+      <Button onClick={handleClick}>Hello</Button>
+      <Greeting />
     </div>
   );
 }
